427958a73f9e92240bc28e2c0de84bf2bc919219
Abdelillahelasri1 <elasriabdelilah68@gmail.com>
2022-10-31 00:01:45 +0100

    modified js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,60 @@
 let produits=[
     {
         "id":"1",
-        "img" : "menu2",
+        "img" : "menu2.jfif",
         "nom" : "sushi tsunada",
         "prix" :"120",
     },
     {
         "id":"2",
-        "img" : "menu3",
+        "img" : "menu3.jfif",
         "nom" : "sushi nagato",
         "prix" :"130",
     },
     {
         "id":"3",
-        "img" : "sushi sasuki",
-        "nom" : "menu4",
+        "img" : "menu4.png",
+        "nom" : "sushi sasuki",
         "prix" :"60",
     },
     {
         "id":"4",
-        "img" : "menu5",
+        "img" : "menu5.jfif",
         "nom" : "sushi obito",
         "prix" :"80",
     },
     {
         "id":"5",
-        "img" : "menu6",
+        "img" : "menu6.jfif",
         "nom" : "sushi roger",
         "prix" :"200",
     },
     {
         "id":"6",
-        "img" : "menu7",
+        "img" : "menu7.jpg",
         "nom" : "sushi gara",
         "prix" :"180",
     },
     {
         "id":"7",
-        "img" : "menu8",
+        "img" : "menu8.jpg",
         "nom" : "sushi naruto",
         "prix" :"220",
     },{
         "id":"8",
-        "img" : "menu9",
+        "img" : "menu9.jfif",
         "nom" : "sushi itachi",
         "prix" :"320",
     },
     {
         "id":"9",
-        "img" : "menu10",
+        "img" : "menu10.png",
         "nom" : "sushi madar",
         "prix" :"20",
     },
     {
         "id":"10",
-        "img" : "menu11",
+        "img" : "menu11.jfif",
         "nom" : "sushi luffy",
         "prix" :"170",
     },
@@ -102,13 +102,33 @@ let produits=[
 
 let btns=document.querySelectorAll(".btn");
 let cart=document.getElementById("cart");
+let total=document.getElementById("total_prix");
+
+let container=0;
+
 btns.forEach(btn=>{
     btn.addEventListener('click',()=>{
         let id=btn.value;
         for(let i=0;i<produits.length;i++){
             if(produits[i].id==id){
-                cart.innerHTML+="<div class="price_test><strong>$170.00</strong><button class="btn" value="10">Select</button></div>";
+                cart.innerHTML+=`
+            <div class="test_produit">
+                <div class="test_produit-image">
+                    <img class="pann-image" width="80" height="80" src="assets/menu/${produits[i].img}" alt="">
+                </div>
+                <div class="test_produit-name-prix">
+                    <h4 style="color: black;">${produits[i].nom}</h4>
+                    <strong style="color: black;">$${produits[i].prix}</strong>
+                </div>
+            </div>
+                `;
+                container += parseFloat(produits[i].prix);
+                total.innerText = 
+                "total : " + container +"hd";
+                console.log(container);
             }
         }
     })
 })
+
+
